8bf47666493a24d714520691f9d4e1ae3f0e68c4
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-06 09:13:33 +0100

    Stretch: Added Carousel functionality | Updated README

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,67 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel(data) {
+	
+	const carousel = document.createElement('div');
+	carousel.classList.add('carousel');
+	
+	carousel.appendChild(createCarouselButton('left-button', ' < ',
+		function() {
+			if(carouselCurrIndex === 0) {
+				carouselCurrIndex = slides.length;
+			}
+			carouselCurrIndex--;
+			updateCarousel();
+		})
+	);
+	data.forEach((slide, i) => {
+		const newSlide = document.createElement('img');
+		newSlide.src = slide;
+		if(i === 0) newSlide.classList.add('active');
+		carousel.appendChild(newSlide);
+	});
+	carousel.appendChild(createCarouselButton('right-button', ' > ',
+		function() {
+			if(carouselCurrIndex === slides.length - 1) {
+				carouselCurrIndex = -1;
+			}
+			carouselCurrIndex++;
+			updateCarousel();
+		})
+	);
+
+	return carousel;
+}
+
+function createCarouselButton(classname, text, onclick) {
+	const btn = document.createElement('div');
+	btn.classList.add(classname);
+	btn.textContent = text;
+	btn.addEventListener('click', onclick);
+	return btn;
+}
+
+function updateCarousel() {
+	const domSlides = document.querySelectorAll('.carousel img');
+	domSlides.forEach((slide, i) => {
+		if(i != carouselCurrIndex) {
+			slide.classList.remove('active');
+		} else {
+			slide.classList.add('active');
+		}
+		slide.style.transform = `translateX(-${carouselCurrIndex * 100}%)`;
+	});
+}
+
+const carouselContainer = document.querySelector('.carousel-container');
+
+let carouselCurrIndex = 0;
+const slides = [
+	"./assets/carousel/mountains.jpeg",
+	"./assets/carousel/computer.jpeg",
+	"./assets/carousel/trees.jpeg",
+	"./assets/carousel/turntable.jpeg"
+];
+carouselContainer.appendChild(createCarousel(slides));
