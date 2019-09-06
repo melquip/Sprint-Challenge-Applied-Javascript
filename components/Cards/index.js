c80093ca66d6038c7106d2677e9aabec9aeca4fa
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-06 08:37:15 +0100

    Completed Cards.js | Updated README

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,38 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container');
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+	.then(response => {
+		Object.keys(response.data.articles).forEach(tab => {
+			response.data.articles[tab].forEach(article => {
+				cardsContainer.appendChild(createCard(article));
+			});
+		});
+	})
+	.catch(error => console.error(error));
+
+function createCard(data) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+	const headline = document.createElement('div');
+	headline.classList.add('headline');
+	headline.textContent = data.headline;
+	const author = document.createElement('div');
+	author.classList.add('author');
+	const imgContainer = document.createElement('div');
+	imgContainer.classList.add('img-container');
+	const img = document.createElement('img');
+	img.src = data.authorPhoto;
+	const authorName = document.createElement('span');
+	authorName.textContent = `By ${data.authorName}`;
+
+	imgContainer.appendChild(img);
+	author.appendChild(imgContainer);
+	author.appendChild(authorName);
+
+	card.appendChild(headline);
+	card.appendChild(author);
+	
+	return card;
+}
